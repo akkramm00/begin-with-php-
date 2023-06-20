40b9fd419d2155646dc7e2852d016b6fc1871a76
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-06-20 16:18:42 +0200

    first commit

---
+++ footer.php
@@ -0,0 +1,7 @@
+
+</main>
+<footer>
+<p>copyright</p>
+</footer>
+</body>
+</html>
---
+++ main.php
@@ -0,0 +1,8 @@
+<h2>Mon amin</h2>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A ut, inventore eos ad iure eius sint sunt, eligendi soluta sapiente ipsam reprehenderit officia ipsum modi consectetur totam voluptate quibusdam. Asperiores?
+Distinctio labore obcaecati id suscipit? Maiores hic quia aperiam iste maxime necessitatibus consequuntur mollitia eos omnis illum, aliquid, adipisci quod sed, delectus tenetur iure sunt? Animi atque quibusdam maiores ipsa!
+Quibusdam assumenda doloribus illum consequuntur sit blanditiis placeat ex eaque minus, culpa magni deserunt rem omnis tempore inventore atque error aut, optio, excepturi debitis? Placeat cum culpa facere architecto molestiae!
+Perspiciatis odio enim quam ullam illum dolor, commodi maiores earum neque placeat ab molestiae, adipisci aut. Iure amet consequatur optio quas voluptatibus debitis sint aperiam labore. Laudantium totam velit similique!
+Magnam voluptatem vero obcaecati ex ut officiis omnis cumque, consequatur porro maxime voluptates inventore accusantium sunt. Non mollitia ex sed repellat! Omnis laboriosam ipsam quasi alias quos qui eos minus!</p>
+
+</main>
